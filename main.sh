d0442760a0c6f61166e7bdc968c4a11ba5b1af30
Lars Falk-Petersen <larsfp@met.no>
2026-01-19 15:06:39 +0100

    Fix sourcing

--- main.sh
+++ main.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+sudo apt install -y --no-install-recommends podman curl libatomic1
 
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-#source ~/.bashrc
-#nvm install node
-#npm install -g @github/copilot
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+. ~/.bashrc
 
-sudo apt install -y --no-install-recommends npm podman
+nvm install node
+npm install -g @github/copilot
+
 
 copilot --allow-all-tools --version
