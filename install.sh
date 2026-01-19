e00178b4b58270686b688aaa035caad162b00d74
Lars Falk-Petersen <larsfp@met.no>
2026-01-19 14:24:16 +0100

    First

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+source ~/.bashrc
+nvm install node
+npm install -g @github/copilot
+copilot --version
