37aa267f045e41c2ca7bbc99f44f04136619b8eb
Lars Falk-Petersen <larsfp@met.no>
2026-01-19 15:02:15 +0100

    podman

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-source ~/.bashrc
-nvm install node
-npm install -g @github/copilot
-copilot --version
---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+#source ~/.bashrc
+#nvm install node
+#npm install -g @github/copilot
+
+sudo apt install -y --no-install-recommends npm podman
+
+copilot --allow-all-tools --version
