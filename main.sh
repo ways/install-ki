f7bb768128c840578f7ec85044c6ac6c9fa28f29
Lars Falk-Petersen <larsfp@met.no>
2026-02-27 14:48:27 +0100

    Update main.sh to install python3-pip and uv
    Added python3-pip installation and uv setup script.

--- main.sh
+++ main.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-sudo apt install -y --no-install-recommends podman curl libatomic1 uidmap slirp4netns
+sudo apt install -y --no-install-recommends podman curl libatomic1 uidmap slirp4netns python3-pip
 
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 . ~/.bashrc
 
+# node & copilot
 nvm install node
 npm install -g @github/copilot
 
