f124efb37d07a9758b7d2d16ce743660d5f506f5
Lars Falk-Petersen <larsfp@met.no>
2026-03-06 10:17:22 +0100

    Update nvm loading in main.sh
    Comment out loading .bashrc and explicitly load nvm.

--- main.sh
+++ main.sh
@@ -7,7 +7,11 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-. ~/.bashrc
+# . ~/.bashrc
+
+# load nvm explicitly!
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # node & copilot
 nvm install node
